d9a4edb1b92a8a0f929bd40be42a26b0a4578b48
Andrew Kennedy <andrewjacobkennedy@gmail.com>
2022-04-02 12:23:51 -0700

    asdf

--- Assets/Scripts/AnimateCoals.cs
+++ Assets/Scripts/AnimateCoals.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class AnimateCoals : MonoBehaviour
 {
-    public Color ColorA;
-    public Color ColorB;
-    public float ColorSpeed = 1f;
-    public float ColorTimerOffset = 0f;
-    
+    public Color ColorA = new Color(1f, .2f, 0f);
+    public Color ColorB = new Color(1f, .4f, 0f);
+    private float _colorSpeed = 1f;
     private float _colorTimer = 0f;
     private int _mainColorId;
 
@@ -18,7 +16,9 @@ public class AnimateCoals : MonoBehaviour
     {
         _material = GetComponent<Renderer>().material;
         _mainColorId = Shader.PropertyToID("_MainColor");
-        _colorTimer = ColorTimerOffset;
+        _colorTimer = Random.Range(0f, 10f);
+
+        transform.rotation = Random.rotation;
     }
 
     void Update()
@@ -29,7 +29,7 @@ public class AnimateCoals : MonoBehaviour
     void AnimateColor()
     {
         _colorTimer += Time.deltaTime;
-        var t = Mathf.Sin(_colorTimer * ColorSpeed);
+        var t = Mathf.Sin(_colorTimer * _colorSpeed);
         t = (t + 1f) * .5f;
         Color newColor = Color.Lerp(ColorA, ColorB, t);
         _material.SetColor(_mainColorId, newColor);
