f36218d08887063ab78b440375127b389127598d
Andrew Kennedy <andrewjacobkennedy@gmail.com>
2022-04-03 21:15:48 -0700

    asfd

--- Assets/Scripts/_old/OldPlayerController.cs
+++ Assets/Scripts/_old/OldPlayerController.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
-using UnityEngine.Rendering.VirtualTexturing;
 using UnityEngine.UIElements;
 
 public class OldPlayerController : MonoBehaviour
