b7fd46913955faa0deb33e0c755abda13836030d
Andrew Kennedy <andrewjacobkennedy@gmail.com>
2022-04-10 16:03:18 -0700

    BoardGen in progress

---
+++ Assets/Scripts/BoardGen.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public static class BoardGen
+{
+    /// <summary>
+    /// Generate a terrain value for a cell.
+    /// </summary>
+    public static Terrain GenerateTerrain(HexCell cell)
+    {
+        return Terrain.Forest;
+    }
+
+    /// <summary>
+    /// Generate a height value for a cell.
+    /// </summary>
+    public static int GenerateHeight(HexCell cell)
+    {
+        return 0;
+    }
+
+    /// <summary>
+    /// Generate a token to appear on a cell.
+    /// </summary>
+    public static void GenerateToken(HexCell cell)
+    {
+        if (Random.Range(0, 10) == 0)
+        {
+            // TokenManager.CreateWood(cell);
+        }
+    }
+}
